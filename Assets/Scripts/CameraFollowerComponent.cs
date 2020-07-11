fc321d222232da04802fd361e6c3510340532ee4
unknown <trevorawestphal@gmail.com>
2020-07-11 07:47:49 -0600

    Added camera following and rough texturing for player ball

---
+++ Assets/Scripts/CameraFollowerComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowerComponent : MonoBehaviour {
+
+    public Rigidbody playerRigidBody;
+
+    public float distanceOffset;
+    public float heightOffset;
+    public float viewHeightOffset;
+
+    public float positionSeekTime;
+    public float viewSeekSpeed;
+
+    private Vector3 cameraVelocity;
+
+    void Update(){
+        // Get ball velocity
+        Vector3 ballVelocity = playerRigidBody.velocity;
+        ballVelocity.y = 0.0f;
+        ballVelocity.Normalize();
+
+        // Find position behind that and up using the offsets
+        Vector3 cameraTargetPosition = playerRigidBody.transform.position - (ballVelocity * distanceOffset);
+        cameraTargetPosition.y += heightOffset;
+
+        // Seek this position and look at ball over time
+        transform.position = Vector3.SmoothDamp(transform.position, cameraTargetPosition, ref cameraVelocity, positionSeekTime);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            Quaternion.LookRotation((playerRigidBody.transform.position + Vector3.up * viewHeightOffset) - transform.position),
+            Time.deltaTime * viewSeekSpeed
+        );
+
+    }
+}
