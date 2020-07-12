fdae48c52c538b4b03f2b2a43d4bbb2401646be2
unknown <trevorawestphal@gmail.com>
2020-07-11 23:52:39 -0600

    zig  zag and s curve tracks added

---
+++ Assets/Scripts/PlayerContactComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContactComponent : MonoBehaviour {
+
+
+
+    void Update(){
+        // Keep track of last time contacting the ground
+        // If too much time (2 seconds?) until last time contacted, fade out and game over sequence
+        // Also stop counting if velocity is very very low
+    }
+}
